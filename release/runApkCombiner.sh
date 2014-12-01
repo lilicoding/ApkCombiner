142aa89804aa63a61d3c9397c1ece84c4e4f0a38
Li <joeyli1989@gmail.com>
2014-12-01 14:35:10 +0100

    minor change of the script

--- release/runApkCombiner.sh
+++ release/runApkCombiner.sh
@@ -6,5 +6,9 @@ cp=$cp:ApkCombiner_lib/jdom-2.0.5.jar
 cp=$cp:ApkCombiner_lib/apktool.jar
 cp=$cp:ApkCombiner.jar
 
+rm -rf workspace
+mkdir workspace
+
 java -cp $cp iccta.apkcombiner.Main $*
 
+rm -rf workspace
